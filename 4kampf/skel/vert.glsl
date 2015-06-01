178cf22436921b677e070f767b09e35761f95075
skomp <r.von.massow@gmail.com>
2015-06-01 19:44:11 +0200

    vertex shader should work with u instead of un too

--- 4kampf/skel/vert.glsl
+++ 4kampf/skel/vert.glsl
@@ -7,7 +7,7 @@
 // Alternatively, see the frag shader comments to see how to calculate
 // ray dir there instead.
 
-uniform vec3 un;		// If Standard Uniforms are enabled, this gives {xres, yres, time}
+uniform vec3 u;		// If Standard Uniforms are enabled, this gives {xres, yres, time}
 // uniform vec3 cp,up,fd;	// If the camera system is enabled, these uniforms give cam position, up and forward vectors, use in the tool
 vec3 cp=vec3(0,0,0),up=vec3(0,1,0),fd=vec3(0,0,1); // workaround for issue in rendering the prod, as there is no cam system export atm
 uniform float ev[6];		// If 4klang envelopes are enabled, this gives envelopes for tracks 0, 1 and 2
@@ -29,5 +29,5 @@ void main(){
 	//#SYNCCODE#
 	
 	vec3 q=cross(fd,up);
-	rd=mat3(q,cross(q,fd),fd)*vec3(gl_Vertex.x*un.x/un.y*scale,gl_Vertex.y*scale,un.y/un.x);
+	rd=mat3(q,cross(q,fd),fd)*vec3(gl_Vertex.x*u.x/u.y*scale,gl_Vertex.y*scale,u.y/u.x);
 }
